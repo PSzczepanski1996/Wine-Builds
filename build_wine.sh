a26cf0fc8113df522a5d1ef2cf38060b1b336389
Patrick Szczepański <p.szczepanski996@gmail.com>
2024-01-22 20:32:11 +0100

    Change path inside build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -61,7 +61,7 @@ export EXPERIMENTAL_WOW64="${EXPERIMENTAL_WOW64:-false}"
 #
 # If you don't want to compile a custom Wine source code, then just leave this
 # variable empty.
-export CUSTOM_SRC_PATH=""
+export CUSTOM_SRC_PATH="$HOME/wine-builds/wine-src"
 
 # Set to true to download and prepare the source code, but do not compile it.
 # If this variable is set to true, root rights are not required.
